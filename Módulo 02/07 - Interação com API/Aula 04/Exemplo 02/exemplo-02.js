a82fe42f51a316b6f888008a895cec698ba20f6c
Gustavo <gustavo.rios801208@gmail.com>
2024-02-06 11:24:40 -0300

    Update

--- Módulo 02/07 - Interação com API/Aula 04/Exemplo 02/exemplo-02.js
+++ Módulo 02/07 - Interação com API/Aula 04/Exemplo 02/exemplo-02.js
@@ -3,9 +3,11 @@ let urlProdutos = "https://raw.githubusercontent.com/Guganp-8012/Meu-Curso-Fulls
 async function buscar(){
     let resposta = await fetch(urlProdutos)
     let produtos = await resposta.json()
-    for (produto in produtos)
+    for (let produto in produtos)
     document.body.innerHTML += `
         <div class="card_produto">
+            <img src="${produtos[produto].img}" width="auto" height="250px">
+
             <h3>
             ${produtos[produto].nome}
             </h3>
