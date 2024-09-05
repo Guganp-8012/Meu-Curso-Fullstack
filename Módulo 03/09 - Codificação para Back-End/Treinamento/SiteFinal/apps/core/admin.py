7cf2de52895c5c42ec9cafda035e5e69a4d90e2a
Guganp-8012 <gustavo.rios801208@gmail.com>
2024-09-05 08:57:38 -0300

    Delete SiteFinal (laravel-api)
    Apagando e organizando para outro repositório

--- Módulo 03/09 - Codificação para Back-End/Treinamento/SiteFinal/apps/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Cliente)
-admin.site.register(Empresa)
-admin.site.register(Categoria)
-admin.site.register(Produto)
-admin.site.register(Servico)
-admin.site.register(OrdemServico)
